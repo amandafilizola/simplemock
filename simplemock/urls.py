d99937d56297ee7f3e4984d8cae1a2f54cb3cd8e
Amanda Filizola <apf@cesar.org.br>
2019-07-24 10:56:08 -0300

    primeiras páginas

--- simplemock/urls.py
+++ simplemock/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^blog/', include('blog.urls')),x
     url(r'^$', 'simplemock.core.views.home', name='home'),
+    url(r'contato/', 'simplemock.core.views.contact', name='contact'),
     url(r'^admin/', include(admin.site.urls)),
 )
